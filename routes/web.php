c99e0f42941505e15882a939856dcaca596a3f17
Gerry Mc Donnell <irishbloke@gmail.com>
2018-02-14 22:38:54 +0000

    auth fix

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,7 @@ Route::resource('todo','TodosController');*/
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
